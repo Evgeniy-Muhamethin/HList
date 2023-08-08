c43ab5b6636b5d36e47b0262401dd6b31e94142d
Evgeniy-Muhamethin <John090918@gmail.com>
2023-08-08 08:23:44 +0300

    Object was created

---
+++ ListOne/ListOne/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ListOne
+{
+	internal class Person
+	{
+		private string firstName;
+		private string lastName;
+		private byte age;
+		private string preferences;
+		private DateTime dateArrival;
+		private DateTime dateDeparture;
+
+		public string FirstName { get { return firstName; } set { firstName = value; } }
+		public string LastName { get { return lastName; } set { lastName = value; } }
+		public byte Age { get { return age;} set { age = value; } }
+		public string Preferences { get { return preferences; } set { preferences = value; } }
+		public DateTime DateArrival { get {  return dateArrival; } set {  dateArrival = value; } }
+		public DateTime DateDeparture { get {  return dateDeparture; } set { dateDeparture = value; } }
+
+		public Person(string firstName, string lastName, byte age, string preferences, 
+			DateTime dateArrival, DateTime dataDeparture) 
+		{ 
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.age = age;
+			this.preferences = preferences;
+			this.dateArrival = dateArrival;
+			this.dateDeparture = dataDeparture;
+		}
+
+		public override string ToString()
+		{
+			return $"{FirstName},\n {LastName},\n {Age},\n {Preferences},\n {DateArrival},\n {DateDeparture}";
+		}
+	}
+}
