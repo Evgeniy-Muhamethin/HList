6fef2ce9cd33b785927a0e9923f255a6258bf01f
Evgeniy-Muhamethin <John090918@gmail.com>
2023-08-28 22:52:21 +0300

    New method was added async

---
+++ ListOne/ListOne/MathOperations.cs
@@ -0,0 +1,41 @@
+﻿namespace ListOne
+{
+	internal class MathOperations
+	{
+		public void StartMathOperation()
+		{
+
+		}
+
+		public void UpdateMathOperation()
+		{
+			//Вызываю метод асинхронного выполнения операции 
+			MathOperationsMethodAsync();
+		}
+
+		//Создаю метод асинхронного выполнения операции 
+		//Это обертка для методы выполняющего операции
+		private async void MathOperationsMethodAsync()
+		{
+			await Task.Run(() => MathOperationsMethod(10,15));
+		}
+
+		//Создаю метод выполняющий операции
+		private void MathOperationsMethod(int numberOne, int numberTwo)
+		{
+			DateTime dateTimeStart = DateTime.Now;
+			int count = int.MaxValue;
+			int countResult = 0;
+			while (count != 0)
+			{
+				int result = numberOne + numberTwo;
+				count--;
+				countResult += result;
+			}
+			DateTime dateTimeStop = DateTime.Now;
+			var timeOperations = dateTimeStart - dateTimeStop;
+            Console.WriteLine($"Count result = {countResult}\n" +
+				$"Time {timeOperations}");
+        }
+	}
+}
--- ListOne/ListOne/Program.cs
+++ ListOne/ListOne/Program.cs
@@ -5,6 +5,8 @@
 		static void Main(string[] args)
 		{
 			MainClass mainClass = new MainClass();
+			MathOperations mathOperations = new MathOperations();
+			mathOperations.UpdateMathOperation();
 			mainClass.Start();
 			mainClass.Update();
 		}
